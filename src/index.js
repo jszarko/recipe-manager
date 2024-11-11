3bc9095515d9fcac0a46c6e85bc9aa35cab7467c
bszarko <64858606+jszarko@users.noreply.github.com>
2024-11-11 14:14:47 -0500

    Addressed package vulnerabilities
    Addressed package vulnerabilities

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 if (process.env.NODE_ENV === 'development') {
-	const { worker } = require('./mocks/browser');
+	// const { worker } = require('./mocks/browser');
 	// worker.start(); // disabled MSW because of security issues with firefox
 }
 
